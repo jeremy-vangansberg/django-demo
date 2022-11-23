cf1431fd165443749c2a01b769b467b4ef6450b7
jeremy Vangansberg <jeremy.vangansberg@gmail.com>
2022-11-23 16:53:37 +0100

    fin de journée

--- functionalities/views.py
+++ functionalities/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import *
 
-# Create your views here.
+class FunctionalitiesListView(ListView):
+    model = Functionalities
+    template_name = 'functionalities/functionalities_list.html'
+    context_object_name = 'func_list'
+
+class FunctionalityDetailView(DetailView):
+    model = Functionalities
+    template_name = 'functionalities/functionality_detail.html'
+    context_object_name = 'functionality'
